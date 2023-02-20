2b99edc34615e1b5271f6d5bfd19f7609dfe99f5
simret <simruineb@gmail.com>
2023-02-20 03:54:21 +0300

    Edit indentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''Page index range'''
 import csv
+import math
 from typing import List, Tuple
 
 
@@ -9,29 +10,30 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = start + page_size
 
-    class Server:
-        """Server class to paginate a database of popular baby names"""
-        DATA_FILE = "Popular_Baby_Names.csv"
 
-        def __init__(self):
-            self.__dataset = None
+class Server:
+    """Server class to paginate a database of popular baby names"""
+    DATA_FILE = "7d3576d97e7560ae85135cc214ffe2b3412c51d7.csv"
 
-            def dataset(self) -> List[List]:
-                """Cached dataset"""
-                if self.__dataset is None:
-                    with open(self.DATA_FILE) as f:
-                        reader = csv.reader(f)
-                        dataset = [row for row in reader]
-                    self.__dataset = dataset[1:]
+    def __init__(self):
+        self.__dataset = None
 
-                return self.__dataset
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-            def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-                '''Dataset pages'''
-                assert type(page) == int and type(page_size) == int
-                assert page > 0 and page_size > 0
-                start, end = index_range(page, page_size)
-                data = self.dataset()
-                if start > len(data):
-                    return []
-                return data[start:end]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Dataset pages'''
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
