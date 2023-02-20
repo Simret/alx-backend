bbaf6af99d9a0694e42b8b3756289767bcbef3bb
simret <simruineb@gmail.com>
2023-02-20 04:00:37 +0300

    Edit indentation

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,6 +9,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''Tuple containing a start index and an end index'''
     start = (page - 1) * page_size
     end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -30,7 +31,7 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''Dataset pages'''
-        assert isinstance(page, int) and isinstance(page_size, int)
+        assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
