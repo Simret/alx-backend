b5af944578d7b9cbdd2cc2789d68df714fa3c5e6
simret <simruineb@gmail.com>
2023-02-23 01:09:00 +0300

    Style

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
 '''LIFO cache'''
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    '''LIFO cache'''
-    current_rank = 0
+    '''Retrieve items'''
 
     def __init__(self):
+        '''Initialize'''
         super().__init__()
-        self.key_rank = {}
-
-    def get_rank(self, order):
-        '''Get the lowest/highest ranked key'''
-        r = sorted(self.key_rank.items(), key=lambda x: x[1], reverse=order)
-        return r[0][0]
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        '''Put item'''
-        d = self.cache_data
-        if key is not None and item is not None:
-            if len(d) < BaseCaching.MAX_ITEMS or key in d:
-                d.update({key: item})
-                self.key_rank.update({key: LIFOCache.current_rank})
-                LIFOCache.current_rank += 1
-            else:
-                rank = self.get_rank(True)
-                d.pop(rank)
-                self.key_rank.pop(rank)
-                d.update({key: item})
-                self.key_rank.update({key: LIFOCache.current_rank})
-                LIFOCache.current_rank += 1
-                print('DISCARD:', rank)
+        '''Add item'''
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        '''Get item'''
-        try:
-            return self.cache_data[key]
-        except Exception:
-            return None
+        '''Retrieve item'''
+        return self.cache_data.get(key, None)
