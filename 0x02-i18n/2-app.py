b7adff115645bab1bc2fd6c8b85ee11d8ff77ba5
simret <simruineb@gmail.com>
2023-03-01 04:15:15 +0300

    Adding local selector

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 '''Flask app'''
-from flask import Flask, render_template
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
 class Config:
-    ''''Flask Babel config'''
+    '''Flask Babel configuration'''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -13,14 +13,19 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''Retrieving locale for a web page'''
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/', strict_slashes=False)
 def get_index() -> str:
     '''Index page'''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
